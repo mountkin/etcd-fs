6c90c13110bcd5ca92746e5cd9d5e8bf893e6112
Shijiang Wei <mountkin@gmail.com>
2016-08-01 15:52:23 +0800

    make it go getable

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/hanwen/go-fuse/fuse/nodefs"
+	"github.com/hanwen/go-fuse/fuse/pathfs"
+	"github.com/mountkin/etcd-fs/etcdfs"
+)
+
+func main() {
+	flag.Parse()
+	if len(flag.Args()) < 2 {
+		log.Fatal("Usage:\n  etcd-fs MOUNTPOINT ETCDENDPOINT")
+	}
+	etcdFs := etcdfs.EtcdFs{FileSystem: pathfs.NewDefaultFileSystem(), EtcdEndpoint: flag.Arg(1)}
+	nfs := pathfs.NewPathNodeFs(&etcdFs, nil)
+	server, _, err := nodefs.MountRoot(flag.Arg(0), nfs.Root(), nil)
+	if err != nil {
+		log.Fatalf("Mount fail: %v\n", err)
+	}
+	server.Serve()
+}
